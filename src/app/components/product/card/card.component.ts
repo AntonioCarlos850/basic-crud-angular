49665eaf75ac39749c7146083550bb198da9ebe9
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-09-07 16:26:17 -0300

    feature/components: product card

---
+++ src/app/components/product/card/card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import Product from 'src/app/interfaces/product';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+  @Input() product: Product = {
+    id: 1,
+    brand: '',
+    description: '',
+    name: '',
+    photo: '',
+    price: 0
+  }
+}
