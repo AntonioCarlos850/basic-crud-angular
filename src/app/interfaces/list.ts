61b135ba97e04e1ccb29ab79fa08db043e47e265
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-09-07 20:01:49 -0300

    refactor/interface/list: remove unused attr

--- src/app/interfaces/list.ts
+++ src/app/interfaces/list.ts
@@ -1,11 +1,6 @@
 export default interface List<T>{
     current_page: number
     data: T[]
-    first_page_url: string
-    from: number
     last_page: number
-    last_page_url: string
-    per_page: number
-    to: number
     total: number
 }
