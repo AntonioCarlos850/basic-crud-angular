87a59f3bf53af200a246d8e7421179cd65cda973
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-09-07 16:26:57 -0300

    feature/routes: registering routes components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CreateComponent } from './components/product/create/create.component';
+import { EditComponent } from './components/product/edit/edit.component';
+import { DeleteComponent } from './components/product/delete/delete.component';
+import { ShowComponent } from './components/product/show/show.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "product/create",
+    component: CreateComponent
+  },
+  {
+    path: "product/edit/:id",
+    component: EditComponent
+  },
+  {
+    path: "product/delete/:id",
+    component: DeleteComponent
+  },
+  {
+    path: "product/show/:id",
+    component: ShowComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
