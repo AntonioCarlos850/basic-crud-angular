8d8a83e5b35c90c3e3b6759614fb184383ce43e6
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-09-07 16:29:27 -0300

    feature/app/module: register app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CardComponent } from './components/product/card/card.component';
+import { CreateComponent } from './components/product/create/create.component';
+import { EditComponent } from './components/product/edit/edit.component';
+import { DeleteComponent } from './components/product/delete/delete.component';
+import { ShowComponent } from './components/product/show/show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    CreateComponent,
+    EditComponent,
+    DeleteComponent,
+    ShowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
